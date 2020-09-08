49462c0d818fcd3c80260a88ee08f9d07858741d
deepthi <deepthin161993@gmail.com>
2020-09-08 12:20:52 +0530

    add chain

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint
+
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var constrainedlog
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -14,7 +16,7 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+    constrainedlog=new Log(230,180,80,PI/2)
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -35,6 +37,18 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    var options={
+      bodyA: bird.body , 
+      bodyB: constrainedlog.body,
+      length:10,
+      stiffness:0.04
+
+
+
+    }
+
+    var chain=Constraint.create(options)
+World.add(world,chain)
 
 }
 
@@ -61,4 +75,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedlog.display()
+    strokeWeight(3)
+    line(bird.body.position.x,bird.body.position.y,constrainedlog.body.position.x,constrainedlog.body.position.y)
 }
